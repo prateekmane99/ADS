efaf8701d102f4ee62e17c3825e3595b2c0e4514
prateekmane99 <Prateek@Prateeks-MacBook-Pro.local>
2016-04-18 03:02:58 -0400

    MANE CODE

--- abc.py
+++ abc.py
@@ -5,90 +5,100 @@ import sys
 
 def main():
 
-    listoflists = []
-    
-    #exampleFile = open('WWW.csv')
-    #exampleReader = csv.reader(exampleFile)
-    #exampleData = list(sys.argv(1))
-    #for row in exampleData:
-    #    listoflists.append(row)
-   
-    
-    #del listoflists[0]
-    values = str(sys.argv[1])
-    header=  str(sys.argv[2])
-    #c = a.split(',', 1)
-    #print(a)
-    #print(b)
+    typee = sys.argv[1]
 
+    if typee == "2":
+        header = sys.argv[2]
+        kopi = str(sys.argv[3])
+        abc = []
+        abc.append(kopi.split(','))
+        header = header.split(',')
+      
 
-    # d = 'hello,world,deryl'
-    # e = str(sys.argv[1])
-    # print(str(sys.argv[1]))
+        data =  {
 
-    # print (type(e))
-    # print(e.split(','))
+            "Inputs": {
 
-    #print(listoflists)
-
-    #print(values.split(','))
-    #print(header.split(','))
-    abc = []
-    abc.append(values.split(','))
-    #print(abc)
-
-    data =  {
-
-        "Inputs": {
-
-                "Input1":
-                {
-                    "ColumnNames" : header.split(','),
-                    "Values": abc #values.split(',')
-                },        },
-            "GlobalParameters": {
-    }
+                    "Input1":
+                    {
+                        "ColumnNames" : header,
+                        "Values": abc #values.split(',')
+                    },        },
+                "GlobalParameters": {
         }
+            }
 
 
-    # print(data)
+        body = str.encode(json.dumps(data))
 
-    body = str.encode(json.dumps(data))
+        url = 'https://ussouthcentral.services.azureml.net/workspaces/8aba4c4d1e034b56941f1f916e884791/services/25e8f06d5e7048bfa64f92cdb249c274/execute?api-version=2.0&details=true'
+        api_key = 'fy4lM67b2VzYJzELzVqqG6zxoodR1OFfi60GbTRabIHaBZfRAJrO1OHhehHKfc8n/TyJnmLGKXQ2JenfPMQUJg=='
 
-   # url = 'https://ussouthcentral.services.azureml.net/workspaces/607045d56e9b4577abd84aab779dc2eb/services/7027c367da5749e3afbf9be9df0f332b/execute?api-version=2.0&details=true'
-    # api_key = 'fH6A1sYHRz5fWa8Q4iHEAZpjXi0uHmK+EIHi7kwwUYueWaLNqIaS/gcr+gqr3QuP3ICgTsIX+4VYH+9+K+ifKg==' # Replace this with the API key for the web service
-    
+        headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-    url = 'https://ussouthcentral.services.azureml.net/workspaces/8aba4c4d1e034b56941f1f916e884791/services/25e8f06d5e7048bfa64f92cdb249c274/execute?api-version=2.0&details=true'
-    api_key = 'fy4lM67b2VzYJzELzVqqG6zxoodR1OFfi60GbTRabIHaBZfRAJrO1OHhehHKfc8n/TyJnmLGKXQ2JenfPMQUJg=='
+        req = urllib2.Request(url, body, headers) 
 
-    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
-
-    req = urllib2.Request(url, body, headers) 
-    # print(req)
-
-    try:
-        response = urllib2.urlopen(req)
+        try:
+            response = urllib2.urlopen(req)
 
         # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
         # req = urllib.request.Request(url, body, headers) 
         # response = urllib.request.urlopen(req)
 
-        result = response.read()
-        print(result)
-        #print('Number of arguments:', len(sys.argv), 'arguments.')
-      #  print 'Argument List:', str(sys.argv)
-        #return result
-        #print("Column Names")
-        #print(json.loads(result)["Results"])
-        # print(dict["Results"])
-    except urllib2.HTTPError, error:
-        print("The request failed with status code: " + str(error.code))
+            result = response.read()
+            print(result)
+        except urllib2.HTTPError, error:
+            print("The request failed with status code: " + str(error.code))
 
         # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-        print(error.info())
+            print(error.info())
 
-        print(json.loads(error.read()))  
+            print(json.loads(error.read()))  
+    else:
+        listoflists = []
+        values = str(sys.argv[1])
+        header=  str(sys.argv[2])
+        abc = []
+        abc.append(values.split(','))
+
+        data =  {
+
+            "Inputs": {
+
+                    "Input1":
+                    {
+                        "ColumnNames" : header.split(','),
+                        "Values": abc #values.split(',')
+                    },        },
+                "GlobalParameters": {
+        }
+            }
+
+        body = str.encode(json.dumps(data))
+
+        url = 'https://ussouthcentral.services.azureml.net/workspaces/8aba4c4d1e034b56941f1f916e884791/services/25e8f06d5e7048bfa64f92cdb249c274/execute?api-version=2.0&details=true'
+        api_key = 'fy4lM67b2VzYJzELzVqqG6zxoodR1OFfi60GbTRabIHaBZfRAJrO1OHhehHKfc8n/TyJnmLGKXQ2JenfPMQUJg=='
+
+        headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+        req = urllib2.Request(url, body, headers) 
+
+        try:
+            response = urllib2.urlopen(req)
+
+        # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+        # req = urllib.request.Request(url, body, headers) 
+        # response = urllib.request.urlopen(req)
+
+            result = response.read()
+            print(result)
+        except urllib2.HTTPError, error:
+            print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+            print(error.info())
+
+            print(json.loads(error.read()))  
         #return json.loads(error.read())
 
 
--- server.js
+++ server.js
@@ -3,16 +3,15 @@ var http = require('http');
 var PythonShell = require('python-shell');
 var ut = require('date-utils');
 
-
 var app = express();
-
-
 
 app.use( express.static(__dirname));
 
 app.get('/', function(req, res) {
 
-	return res.sendFile(__dirname + '/god3.html');
+	return res.sendFile(__dirname + '/Design/pages/index.html');
+    //return res.sendFile(__dirname + '/indi.html');
+
 });
 
 
@@ -20,31 +19,71 @@ app.get('/', function(req, res) {
 
 app.get('/query1' , function(req, res) {
 
-	var row_1 = req.param('d');	
-	var header = req.param('h');	
-	console.log(row_1);
+  var type = req.param('v6'); 
 
-	console.log(row_1.split(','));
+  if(type == "2") 
+  {
+      var header = ["Account", "variable", "kWH", "df$date", "df$month"];
+      var acc = req.param('v1'); 
+      var variable = req.param('v2'); 
+      var kw = req.param('v3'); 
+      var d = req.param('v4'); 
+      var mnth = req.param('v5'); 
 
-	var options = {
+      var valuess = new Array();
+      valuess.push(acc);
+      valuess.push(variable);
+      valuess.push(kw);
+      valuess.push(d);
+      valuess.push(mnth);
+
+    var options = {
+      //mode: 'text',
+      //args: [type, header, acc, variable, kw, d, mnth]
+      args: [type, header, valuess]
+    };
+
+    PythonShell.run('abc.py', options, function (err, results) {
+      if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    //console.log(results)
+    output = JSON.parse(results);
+    var scoredLabels = output.Results.BostonPropertyOutput.value.Values[0][0];
+    //var scoreProb = output.Results.BostonPropertyOutput.value.Values[0][1];
+    //var scoreProb = 0;
+
+   // console.log('POP %j', output);
+    //res.send(scoredLabels+','+scoreProb);
+    res.send(scoredLabels);
+    
+  });
+
+  }
+  else 
+  {
+   var row_1 = req.param('d');	
+   var header = req.param('h');	
+   
+   var options = {
   		//mode: 'text',
   		args: [row_1.split(','), header.split(',')]
-	};
+   };
 
-	PythonShell.run('abc.py', options, function (err, results) {
-  	if (err) throw err;
+   PythonShell.run('abc.py', options, function (err, results) {
+     if (err) throw err;
   	// results is an array consisting of messages collected during execution
   	console.log(results)
   	output = JSON.parse(results);
   	var scoredLabels = output.Results.BostonPropertyOutput.value.Values[0][0];
-  	// var scoreProb = output.Results.BostonPropertyOutput.value.Values[0][1];
+    var scoreProb = output.Results.BostonPropertyOutput.value.Values[0][1];
     var scoreProb = 0;
 
-  	console.log('POP %j', output);
-  	res.send(row_1+','+scoredLabels+','+scoreProb);
-  	
-	});
+    console.log('POP %j', output);
+    res.send(row_1+','+scoredLabels+','+scoreProb);
 
+  });
+
+ }
 
 });
 
